bc1ba12474ac33ab93ac6b39b191fbe367f5b3bb
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2023-01-09 00:19:38 +0800

    Add icon
    feat: add icon
    
    feat: add oidc research playground
    
    report: freeze for submission

--- go.mod
+++ go.mod
@@ -24,9 +24,12 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
 	github.com/dlclark/regexp2 v1.2.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -35,7 +38,13 @@ require (
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
@@ -47,10 +56,18 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/mattn/goveralls v0.0.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/ory/fosite v0.42.3-0.20220508104133-3a0c9520791b // indirect
+	github.com/ory/fosite-example v0.0.0-20220508105812-99739a5bc1c6 // indirect
+	github.com/ory/go-acc v0.2.6 // indirect
+	github.com/ory/go-convenience v0.1.0 // indirect
 	github.com/ory/pagination v0.0.1 // indirect
+	github.com/ory/viper v1.7.5 // indirect
+	github.com/ory/x v0.0.214 // indirect
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
@@ -65,10 +82,16 @@ require (
 	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/zitadel/logging v0.3.4 // indirect
+	github.com/zitadel/oidc v1.12.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ playground/oidc_op_gin/oidc_op_gin.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"github.com/zitadel/oidc/example/server/storage"
+	"github.com/zitadel/oidc/pkg/op"
+	"golang.org/x/text/language"
+	"ingress-authproxy/playground/oidc_op_gin/exampleop"
+	"net/http"
+	"os"
+)
+
+const pathLoggedOut = "/logged-out"
+
+// newOP will create an OpenID Provider for localhost on a specified port with a given encryption key
+// and a predefined default logout uri
+// it will enable all options (see descriptions)
+func newOP(ctx context.Context, storage op.Storage, issuer string, key [32]byte) (op.OpenIDProvider, error) {
+	config := &op.Config{
+		Issuer:    issuer,
+		CryptoKey: key,
+
+		// will be used if the end_session endpoint is called without a post_logout_redirect_uri
+		DefaultLogoutRedirectURI: pathLoggedOut,
+
+		// enables code_challenge_method S256 for PKCE (and therefore PKCE in general)
+		CodeMethodS256: true,
+
+		// enables additional client_id/client_secret authentication by form post (not only HTTP Basic Auth)
+		AuthMethodPost: true,
+
+		// enables additional authentication by using private_key_jwt
+		AuthMethodPrivateKeyJWT: true,
+
+		// enables refresh_token grant use
+		GrantTypeRefreshToken: true,
+
+		// enables use of the `request` Object parameter
+		RequestObjectSupported: true,
+
+		// this example has only static texts (in English), so we'll set the here accordingly
+		SupportedUILocales: []language.Tag{language.English},
+	}
+	handler, err := op.NewOpenIDProvider(ctx, config, storage,
+		// as an example on how to customize an endpoint this will change the authorization_endpoint from /authorize to /auth
+		op.WithCustomAuthEndpoint(op.NewEndpoint("auth")),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return handler, nil
+}
+
+// setupLoginServer creates an OIDC server with Issuer=http://localhost:<port>
+//
+// Use one of the pre-made clients in storage/clients.go or register a new one.
+func setupLoginServer(storage *storage.Storage, provider op.OpenIDProvider) *mux.Router {
+
+	router := mux.NewRouter()
+
+	// for simplicity, we provide a very small default page for users who have signed out
+	router.HandleFunc(pathLoggedOut, func(w http.ResponseWriter, req *http.Request) {
+		_, err := w.Write([]byte("signed out successfully"))
+		if err != nil {
+			log.Printf("error serving logged out page: %v", err)
+		}
+	})
+
+	// the provider will only take care of the OpenID Protocol, so there must be some sort of UI for the login process
+	// for the simplicity of the example this means a simple page with username and password field
+	l := exampleop.NewLogin(storage, op.AuthCallbackURL(provider))
+
+	// regardless of how many pages / steps there are in the process, the UI must be registered in the router,
+	// so we will direct all calls to /login to the login UI
+	router.PathPrefix("/login/").Handler(http.StripPrefix("/login", l.Router))
+
+	// we register the http handler of the OP on the root, so that the discovery endpoint (/.well-known/openid-configuration)
+	// is served on the correct path
+	//
+	// if your issuer ends with a path (e.g. http://localhost:9998/custom/path/),
+	// then you would have to set the path prefix (/custom/path/)
+	router.PathPrefix("/").Handler(provider.HttpHandler())
+
+	return router
+}
+
+func setupOIDCServer(ctx context.Context, issuer string, storage op.Storage, encryptionKey string) op.OpenIDProvider {
+	// this will allow us to use an issuer with http:// instead of https://
+	_ = os.Setenv(op.OidcDevMode, "true")
+
+	// the OpenID Provider requires a 32-byte key for (token) encryption
+	// be sure to create a proper crypto random key and manage it securely!
+	key := sha256.Sum256([]byte(encryptionKey))
+
+	// for simplicity, we provide a very small default page for users who have signed out
+
+	// creation of the OpenIDProvider with the just created in-memory Storage
+	provider, err := newOP(ctx, storage, issuer, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return provider
+}
+
+func main() {
+	ctx := context.Background()
+	// the OpenIDProvider interface needs a Storage interface handling various checks and state manipulations
+	// this might be the layer for accessing your database
+	// in this example it will be handled in-memory
+	repo := storage.NewStorage(storage.NewUserStore())
+	port := "9998"
+
+	provider := setupOIDCServer(ctx, "http://localhost:"+port, repo, "encryptionKey")
+	router := setupLoginServer(repo, provider)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-ctx.Done()
+}
